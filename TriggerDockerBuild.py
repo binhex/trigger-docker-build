f1a5949b260704352331e3b4133a28bfac9a70a3
binhex <megalith01@gmail.com>
2017-03-23 13:37:12 +0000

    more initial changes

--- TriggerDockerBuild.py
+++ TriggerDockerBuild.py
@@ -1,12 +1,19 @@
 import requests
 import configobj
 import os
+import sys
 import socket
 import logging
 import logging.handlers
 import backoff
 import json
 import yagmail
+import schedule
+import time
+
+# ensure we correctly handle all keyboard interrupts
+import signal
+signal.signal(signal.SIGINT, signal.default_int_handler)
 
 # required to suppress ssl warning for urllib3 (requests uses urllib3)
 import requests.packages.urllib3
@@ -98,7 +105,7 @@ def app_logging():
 app_log = app_logging()
 
 
-def notification_email(app_name, repo_name):
+def notification_email(app_name, repo_name, target_repo_name, previous_version, current_version):
 
     # read email config
     config_email_username = config_obj["notification"]["email_username"]
@@ -106,16 +113,22 @@ def notification_email(app_name, repo_name):
     config_email_to = config_obj["notification"]["email_to"]
 
     # construct url to docker hub build details
-    config_github_owner = config_obj["general"]["github_owner"]
-    dockerhub_build_details = "https://hub.docker.com/r/%s/arch-%s/builds/" % (config_github_owner, app_name)
+    config_target_repo_owner = config_obj["general"]["target_repo_owner"]
+    dockerhub_build_details = "https://hub.docker.com/r/%s/%s/builds/" % (config_target_repo_owner, target_repo_name)
 
     app_log.info(u'Sending email notification...')
 
     yag = yagmail.SMTP(config_email_username, config_email_password)
-    subject = 'API/URL triggered build'
-    body = 'Release triggered for app %s at repo %s' % (app_name, repo_name)
-    html = 'Link to Docker Hub build page %s' % dockerhub_build_details
-    yag.send(to = config_email_to, subject = subject, contents = [body, html])
+    subject = 'Trigger Build: [%s] - version changed from %s to %s' % (app_name, previous_version, current_version)
+    html = '''
+    <b>Status:</b> Release triggered<br>
+    <b>Application:</b> %s<br>
+    <b>Repository:</b> %s<br>
+    <b>Previous Version:</b> %s<br>
+    <b>Current Version:</b> %s<br>
+    <b>Docker Hub:</b> <a href="%s">Build Details</a><br>
+    ''' % (app_name, repo_name, previous_version, current_version, dockerhub_build_details)
+    yag.send(to = config_email_to, subject = subject, contents = [html])
 
 
 @backoff.on_exception(backoff.expo, (socket.timeout, requests.exceptions.Timeout, requests.exceptions.HTTPError),
@@ -131,15 +144,24 @@ def http_client(**kwargs):
         else:
 
             app_log.warning(u'No URL sent to function, exiting function...')
-            return 1
+            return 1, None
 
         if "user_agent" in kwargs:
             user_agent = kwargs['user_agent']
 
         else:
 
-            app_log.warning(u'No Usent Agent sent to function, exiting function...')
-            return 1
+            app_log.warning(u'No User Agent sent to function, exiting function...')
+            return 1, None
+
+        # optional stuff to include
+        if "auth" in kwargs:
+
+            auth = kwargs['auth']
+
+        else:
+
+            auth = None
 
         if "additional_header" in kwargs:
 
@@ -190,17 +212,69 @@ def http_client(**kwargs):
         if "data_payload" in kwargs:
 
             # request url get with timeouts and custom headers
-            response = session.post(url=url, timeout=(connect_timeout, read_timeout), headers=headers, allow_redirects=True, verify=False, data=data_payload)
+            response = session.post(url=url, auth=auth, timeout=(connect_timeout, read_timeout), headers=headers, allow_redirects=True, verify=False, data=data_payload)
 
         else:
 
             # request url get with timeouts and custom headers
-            response = session.get(url=url, timeout=(connect_timeout, read_timeout), headers=headers, allow_redirects=True,
+            response = session.get(url=url, auth=auth, timeout=(connect_timeout, read_timeout), headers=headers, allow_redirects=True,
                                    verify=False)
 
         # get status code and content downloaded
         status_code = response.status_code
         content = response.content
+
+        if status_code == 401:
+
+            app_log.warning(
+                u"The status code %s indicates the github personal token is revoked %s, error is %s" % (status_code, url, content))
+            raise requests.exceptions.HTTPError
+
+        elif status_code == 404:
+
+            app_log.warning(u"The status code %s indicates the remote site is down for %s, error is %s" % (status_code, url, content))
+            raise requests.exceptions.HTTPError
+
+        elif status_code == 422:
+
+            app_log.warning(u"The status code %s indicates a duplicate release already exists on github for %s, error is %s" % (status_code, url, content))
+            raise requests.exceptions.HTTPError
+
+        elif status_code != 200 and status_code != 201:
+
+            app_log.warning(u"The status code %s indicates an unexpected error for %s, error is %s" % (status_code, url, content))
+            raise requests.exceptions.HTTPError
+
+    except requests.exceptions.ConnectTimeout as content:
+
+        # connect timeout occurred
+        app_log.warning(u"Connection timed for URL %s with error %s" % (url, content))
+        return 1, content
+
+    except requests.exceptions.ConnectionError as content:
+
+        # connection error occurred
+        app_log.warning(u"Connection error for URL %s with error %s" % (url, content))
+        return 1, content
+
+    except requests.exceptions.TooManyRedirects as content:
+
+        # too many redirects, bad site or circular redirect
+        app_log.warning(u"Too many retries for URL %s with error %s" % (url, content))
+        return 1, content
+
+    except requests.exceptions.HTTPError:
+
+        # catch http exceptions thrown by requests
+        return 1, content
+
+    except requests.exceptions.RequestException as content:
+
+        # catch any other exceptions thrown by requests
+        app_log.warning(u"Caught other exceptions for URL %s with error %s" % (url, content))
+        return 1, content
+
+    else:
 
         # if status code is not 200 and not 404 (file not found) then raise exception to cause backoff
         if status_code == 200:
@@ -214,61 +288,19 @@ def http_client(**kwargs):
             app_log.info(u"The status code %s indicates a successful creation of a github release for %s" % (status_code, url))
             return 0, content
 
-        elif status_code == 404:
 
-            app_log.warning(u"The status code %s indicates the remote site is down for %s" % (status_code, url))
-            raise requests.exceptions.HTTPError
-
-        elif status_code == 422:
-
-            app_log.warning(u"The status code %s indicates a duplicate release already exists on github for %s" % (status_code, url))
-            raise requests.exceptions.HTTPError
-
-    except requests.exceptions.ConnectTimeout:
-
-        # connect timeout occurred
-        app_log.warning(u"Index site feed/api download connect timeout for %s" % url)
-        return 1, content
-
-    except requests.exceptions.ConnectionError as e:
-
-        # connection error occurred
-        app_log.warning(u"Index site feed/api download connection error %s for %s" % (e, url))
-        return 1, content
-
-    except requests.exceptions.TooManyRedirects:
-
-        # too many redirects, bad site or circular redirect
-        app_log.warning(u"Index site feed/api download too many redirects for %s" % url)
-        return 1, content
-
-    except requests.exceptions.HTTPError:
-
-        # catch http exceptions thrown by requests
-        return 1, content
-
-    except requests.exceptions.RequestException:
-
-        # catch any other exceptions thrown by requests
-        app_log.warning(u"Index site feed/api download failed, giving up for %s" % url)
-        return 1, content
-
-
-def github_create_release(current_version, app_name):
-
-    config_github_owner = config_obj["general"]["github_owner"]
-    config_github_access_token = config_obj["general"]["github_access_token"]
+def github_create_release(current_version, target_repo_owner, target_repo_name, target_access_token):
 
     app_log.info(u"Creating Release on GitHub for version %s..." % current_version)
 
-    github_owner = config_github_owner
-    github_repo = "arch-%s" % app_name
-    github_access_token = config_github_access_token
+    target_repo_owner = target_repo_owner
+    target_repo_name = target_repo_name
+    target_access_token = target_access_token
     github_tag_name = "%s-01" % current_version
     github_release_name = "API/URL triggered release"
     github_release_body = github_tag_name
     http_request_type = "post"
-    http_url = 'https://api.github.com/repos/%s/%s/releases?access_token=%s' % (github_owner, github_repo, github_access_token)
+    http_url = 'https://api.github.com/repos/%s/%s/releases?access_token=%s' % (target_repo_owner, target_repo_name, target_access_token)
     http_data_payload = '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "%s","draft": false,"prerelease": false}' % (github_tag_name, github_release_name, github_release_body)
 
     # process post request
@@ -278,28 +310,30 @@ def github_create_release(current_version, app_name):
 
 def monitor_sites():
 
-    app_log.info(u"Monitoring sites for application version changes...")
-
     # read sites list from config
     config_site_list = config_obj["monitor_sites"]["site_list"]
+    target_repo_owner = config_obj["general"]["target_repo_owner"]
+    target_access_token = config_obj["general"]["target_access_token"]
 
     # loop over each site and check previous and current result
     for site_item in config_site_list:
 
-        scm_name = site_item["scm_name"]
-        app_name = site_item["app_name"]
-        repo_name = site_item["repo_name"]
+        source_site_name = site_item["source_site_name"]
+        source_app_name = site_item["source_app_name"]
+        source_repo_name = site_item["source_repo_name"]
+        target_repo_name = site_item["target_repo_name"]
 
-        app_log.info(u"Processing started for application %s..." % app_name)
+        app_log.info(u"-------------------------------------")
+        app_log.info(u"Processing started for application %s..." % source_app_name)
 
-        if scm_name == "github":
+        if source_site_name == "github":
 
             # use github rest api to get app release info
-            url = "https://api.github.com/repos/%s/%s/tags" % (app_name, repo_name)
+            url = "https://api.github.com/repos/%s/%s/tags" % (source_repo_name, source_app_name)
             request_type = "get"
 
             # download webpage content
-            status_code, content = http_client(url=url, user_agent=user_agent_chrome, request_type=request_type)
+            status_code, content = http_client(url=url, auth=('binhex', target_access_token), user_agent=user_agent_chrome, request_type=request_type)
 
             if status_code == 0:
 
@@ -307,16 +341,22 @@ def monitor_sites():
 
             else:
 
-                app_log.info(u"Problem downloading app info, skipping to new release...")
+                app_log.info(u"[ERROR] Problem downloading app info, skipping to new release...")
                 continue
 
-            # get tag name from json
-            current_version = content[0]['name']
+            try:
 
-        elif scm_name == "aor":
+                # get tag name from json
+                current_version = content[0]['name']
+
+            except IndexError:
+
+                current_version = "0.0"
+
+        elif source_site_name == "aor":
 
             # use aor unofficial api to get app release info
-            url = "https://www.archlinux.org/packages/search/json/?q=%s&arch=any&arch=x86_64" % app_name
+            url = "https://www.archlinux.org/packages/search/json/?q=%s&arch=any&arch=x86_64" % source_app_name
             request_type = "get"
 
             # download webpage content
@@ -328,22 +368,28 @@ def monitor_sites():
 
             else:
 
-                app_log.info(u"Problem downloading app info, skipping to new release...")
+                app_log.info(u"[ERROR] Problem downloading app info, skipping to new release...")
                 continue
 
-            # get package version and release number from json
-            pkgver = content['results'][0]['pkgver']
-            pkgrel = content['results'][0]['pkgrel']
+            try:
 
-            # construct app version
-            current_version = "%s-%s" % (pkgver, pkgrel)
+                # get package version and release number from json
+                pkgver = content['results'][0]['pkgver']
+                pkgrel = content['results'][0]['pkgrel']
 
-            last_update = content['results'][0]['last_update']
+                # construct app version
+                current_version = "%s-%s" % (pkgver, pkgrel)
 
-        elif scm_name == "aur":
+                last_update = content['results'][0]['last_update']
+
+            except IndexError:
+
+                current_version = "0.0"
+
+        elif source_site_name == "aur":
 
             # use aur api to get app release info
-            url = "https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=%s" % app_name
+            url = "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=%s" % source_app_name
             request_type = "get"
 
             # download webpage content
@@ -355,59 +401,79 @@ def monitor_sites():
 
             else:
 
-                app_log.info(u"Problem downloading app info, skipping to new release...")
+                app_log.info(u"[ERROR] Problem downloading app info, skipping to new release...")
                 continue
 
-            # get app version from json
-            current_version = content["results"][0]["Version"]
+            try:
+
+                # get app version from json
+                current_version = content["results"][0]["Version"]
+
+            except IndexError:
+
+                current_version = "0.0"
 
         # write value for current match to config
-        config_obj["monitor_sites"]["%s_%s_current_version" % (scm_name, app_name)] = current_version
+        config_obj["results"]["%s_%s_current_version" % (source_site_name, source_app_name)] = current_version
         config_obj.write()
 
         try:
 
             # read value from previous match from config
-            previous_version = config_obj["monitor_sites"]["%s_%s_previous_version" % (scm_name, app_name)]
+            previous_version = config_obj["results"]["%s_%s_previous_version" % (source_site_name, source_app_name)]
 
         except KeyError:
 
-            app_log.info(u"No known previous version for app %s, assuming first run" % app_name)
+            app_log.info(u"No known previous version for app %s, assuming first run" % source_app_name)
             app_log.info(u"Setting previous version to current version %s and going to next iteration" % current_version)
-            config_obj["monitor_sites"]["%s_%s_previous_version" % (scm_name, app_name)] = current_version
+            config_obj["results"]["%s_%s_previous_version" % (source_site_name, source_app_name)] = current_version
             config_obj.write()
             continue
 
         if previous_version != current_version:
 
-            app_log.info(u"Current version %s and previous version %s are different, triggering a docker hub build (via github tag)..." % (current_version, previous_version))
-            status_code, content = github_create_release(current_version, app_name)
+            app_log.info(u"[TRIGGER] Previous version %s and current version %s are different, triggering a docker hub build (via github tag)..." % (previous_version, current_version))
+            status_code, content = github_create_release(current_version, target_repo_owner, target_repo_name, target_access_token)
 
             if status_code == 0:
 
                 app_log.info(u"Setting previous version %s to the same as current version %s after successful build" % (previous_version, current_version))
-                config_obj["monitor_sites"]["%s_%s_previous_version" % (scm_name, app_name)] = current_version
+                config_obj["results"]["%s_%s_previous_version" % (source_site_name, source_app_name)] = current_version
                 config_obj.write()
 
                 # send email notification
-                notification_email(app_name, repo_name)
+                notification_email(source_app_name, source_repo_name, target_repo_name, previous_version, current_version)
 
             else:
 
-                app_log.info(u"Problem creating github release and tag, skipping to next iteration...")
+                app_log.warning(u"[ERROR] Problem creating github release and tag, skipping to next iteration...")
                 continue
 
         else:
 
-            app_log.info(u"Current version %s and previous version %s match, nothing to do" % (current_version, previous_version))
+            app_log.info(u"[SKIPPED] Previous version %s and current version %s match, nothing to do" % (previous_version, current_version))
 
-        app_log.info(u"Processing finished for application %s" % app_name)
+        app_log.info(u"Processing finished for application %s" % source_app_name)
 
-    app_log.info(u"All applications processed")
+    app_log.info(u"All applications processed, waiting for next invocation...")
 
 # required to prevent separate process from trying to load parent process
 if __name__ == '__main__':
 
-    app_log.info(u"Starting monitoring...")
-    monitor_sites()
-    app_log.info(u"Finished monitoring, exiting script...")
+    app_log.info(u"Monitoring sites for application version changes...")
+
+    config_schedule_check_mins = config_obj["general"]["schedule_check_mins"]
+    app_log.info(u"Checking for changes every %s minutes..." % config_schedule_check_mins)
+    schedule.every(config_schedule_check_mins).minutes.do(monitor_sites)
+
+    while True:
+
+        try:
+
+            schedule.run_pending()
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+
+            app_log.info(u"Keyboard interrupt received, exiting script...")
+            sys.exit()
