b75faeb584482155cc82c0125c7a6892d71fa41d
binhex <megalith01@gmail.com>
2022-06-29 13:55:32 +0100

    typo on cli for gitlab

--- TriggerDockerBuild.py
+++ TriggerDockerBuild.py
@@ -1254,7 +1254,7 @@ if __name__ == '__main__':
     commandline_parser.add_argument(u"--kodi-password", metavar=u"<password>", help=u"specify the password to access kodi e.g. --kodi-password foo")
     commandline_parser.add_argument(u"--email-password", metavar=u"<password>", help=u"specify the email account password e.g. --email-password foo")
     commandline_parser.add_argument(u"--target-access-token", metavar=u"<token>", help=u"specify the github personal access token e.g. --target-access-token 123456789")
-    commandline_parser.add_argument(u"--gitlab_access_token", metavar=u"<token>", help=u"specify the gitlab personal access token e.g. --gitlab_access_token 123456789")
+    commandline_parser.add_argument(u"--gitlab-access-token", metavar=u"<token>", help=u"specify the gitlab personal access token e.g. --gitlab-access-token 123456789")
     commandline_parser.add_argument(u"--kodi-notification", action=u"store_true", help=u"enable kodi notification e.g. --kodi-notification")
     commandline_parser.add_argument(u"--email-notification", action=u"store_true", help=u"enable email notification e.g. --email-notification")
     commandline_parser.add_argument(u"--pidfile", metavar=u"<path>", help=u"specify path to pidfile e.g. --pid /var/run/triggerdockerbuild/triggerdockerbuild.pid")
